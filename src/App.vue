d0146d9a572e9a0d884dea7422786a07b84990c4
Margaret Mwaura <mwauramargaret1@gmail.com>
2019-12-31 23:38:11 +0300

    code sample

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div id="app_rou">
+            <div id="app_rou_inner">
+                <router-link to="/post" id="thing">Home</router-link>
+                <router-link to="/first" id="thingt">About us</router-link>
+            </div>
+
+        </div>
+        <router-view></router-view>
+        <notifications group="foo" />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    export default {
+        name: 'app',
+        data (){
+            return {
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+            }
+        },
+        methods: {
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
+import  store  from '../store';
+import VueRouter from 'vue-router';
+import Notifications from 'vue-notification'
+Vue.use(Notifications);
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+import posting from './components/posting';
+import first from './components/first';
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/post',
+            component: posting,
+        },
+        {
+            path: '/first',
+            component: first,
+
+        },
+    ],
+});
+
 
 new Vue({
+    store,
+    router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
